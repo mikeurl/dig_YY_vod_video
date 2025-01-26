31393b7c8aa557fb8ba276848a63a8ffe3db9624
mikeurl <153970945+mikeurl@users.noreply.github.com>
2025-01-25 20:30:33 -0800

    Update pipelines.py

--- tutorial/pipelines.py
+++ tutorial/pipelines.py
@@ -12,14 +12,19 @@ class TutorialPipeline(object):
     def process_item(self, item, spider):
         try:
             with open( item['data_anchor'] + '.txt', 'r') as fp:
-                if item['data_pid'] in fp.read().decode('utf8'):
-                    return item 
-        except IOError as e:
-            if e.errno == 2:
-                log.msg(item['data_anchor'] + '.txt not be created', level='WARNING')
-        with open( item['data_anchor'] + '.txt', 'a+') as fp:
-            if item['data_yyNo'] and item['data_pid']:
-                fp.write(('|'.join(item.get_ex_record()) + '\n').encode('utf8'))
-            else:
-                fp.write(('|'.join(item.get_record()) + '\n').encode('utf8'))
-        return item
+       def process_item(self, item, spider):
+    try:
+        with open(item['data_anchor'] + '.txt', 'r', encoding='utf-8') as fp:
+            if item['data_pid'] in fp.read():
+                return item
+    except IOError as e:
+        if e.errno == 2:
+            log.msg(f'Item {item["data_anchor"]}.txt not created', level='WARNING')
+    
+    with open(item['data_anchor'] + '.txt', 'a', encoding='utf-8') as fp:
+        if 'data_pid' in item:
+            fp.write('|'.join(item.get_ex_record()) + '\n')
+        else:
+            fp.write('|'.join(item.get_record()) + '\n')
+
+    return item
